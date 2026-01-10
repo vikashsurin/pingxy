9a226446c2a30c0d20ad9f33d0e30f9a7948a403
VikashSurin <vikashsurin10@gmail.com>
2026-01-11 03:35:41 +0530

    code clean up

--- apps/backend/src/core/utils/index.ts
+++ apps/backend/src/core/utils/index.ts
@@ -1,34 +1,6 @@
-import {
-  type Connection,
-  connectionSchema,
-  Message,
-  messageSchema,
-} from "@chat/shared/src/lib/utils/tempp.js";
 import { extendSessionActivity, getSessionUser } from "../../features/sessions";
 
 
-
-export function validateConnection(connection: Connection) {
-  const validateConnection = connectionSchema.safeParse(connection);
-
-  if (!validateConnection.success) {
-    return;
-  }
-  const validConnection = validateConnection.data;
-  return validConnection;
-}
-
-// function to validate messages
-export function validateMessage(message: Message) {
-  const validateMessage = messageSchema.safeParse(message);
-
-  if (!validateMessage.success) {
-    return;
-  }
-
-  const validMessage = validateMessage.data;
-  return validMessage;
-}
 
 export async function getAuthUserFromReq(req: Request) {
   const cookieHeader = req.headers.get("cookie");
