f822934c9855fb10c848ae1f07ac2f19fd9ad9c8
VikashSurin <vikashsurin10@gmail.com>
2026-01-31 15:17:52 +0530

    added constants for user

---
+++ packages/shared/src/constants/user.ts
@@ -0,0 +1,10 @@
+export const MIN_NAME_LENGTH = 3;
+export const MAX_NAME_LENGTH = 16;
+
+export const MIN_PASSWORD_LENGTH = 8;
+export const MAX_PASSWORD_LENGTH = 32;
+
+export const MIN_AGE = 18;
+export const MAX_AGE = 100;
+
+export const MAX_BIO_LENGTH = 160;
