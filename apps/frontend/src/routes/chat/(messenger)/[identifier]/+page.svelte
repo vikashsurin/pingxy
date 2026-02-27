2ac2aadef33a5cf2a6749ef830631dd7cbee3fee
VikashSurin <vikashsurin10@gmail.com>
2026-02-27 23:03:36 +0530

    added optional pages

--- apps/frontend/src/routes/chat/(messenger)/[identifier]/+page.svelte
+++ apps/frontend/src/routes/chat/(messenger)/[identifier]/+page.svelte
@@ -1,170 +1,148 @@
 <script lang="ts">
-    import { validateSocket } from "$lib/store/helpers.js";
-    import { emitMarkAllRead } from "$lib/store/managers/entities/receipt.svelte.js";
-    import { messageStore } from "$lib/store/messageStore.svelte.js";
-    import { formatLocalTime } from "$lib/utils/time.js";
-    import { Check, CheckCheck } from "@lucide/svelte";
-    import { type MessageReceipt } from "@pingxy/shared";
-    import ChatHeader from "./ChatHeader.svelte";
-    import ChatInput from "./ChatInput.svelte";
+  import { validateSocket } from "$lib/store/helpers.js";
+  import { emitMarkAllRead } from "$lib/store/managers/entities/receipt.svelte.js";
+  import { messageStore } from "$lib/store/messageStore.svelte.js";
+  import { formatLocalTime } from "$lib/utils/time.js";
+  import { Check, CheckCheck } from "@lucide/svelte";
+  import { type MessageReceipt } from "@pingxy/shared";
+  import ChatHeader from "./ChatHeader.svelte";
+  import ChatInput from "./ChatInput.svelte";
 
-    let { data } = $props();
+  let { data } = $props();
 
-    $effect(() => {
-        if (data.messages.items) {
-            messageStore.setMessages(data.messages.items);
-        }
-    });
+  $effect(() => {
+    if (data.messages.items) {
+      messageStore.setMessages(data.messages.items);
+    }
+  });
 
-    const messageIds = $derived(
-        Array.from(messageStore.threads.get(data.idValue) || []),
-    );
+  const messageIds = $derived(
+    Array.from(messageStore.threads.get(data.idValue) || []),
+  );
 
-    const interSectionObserver = (node: HTMLElement, callback: () => void) => {
-        const observer = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                callback();
-            }
-        }, {});
+  const interSectionObserver = (node: HTMLElement, callback: () => void) => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        callback();
+      }
+    }, {});
 
-        observer.observe(node);
-        return {
-            destroy() {
-                observer.disconnect();
-            },
-        };
+    observer.observe(node);
+    return {
+      destroy() {
+        observer.disconnect();
+      },
     };
+  };
 
-    function loadOlder() {
-        console.log("loading older");
-    }
+  function loadOlder() {
+    console.log("loading older");
+  }
 
-    let messageListRef: HTMLElement | undefined;
+  let messageListRef: HTMLElement | undefined;
 
-    $effect(() => {
-        if (!data.identifier.startsWith("c_")) return;
+  $effect(() => {
+    if (!data.identifier.startsWith("c_")) return;
 
-        const conversationId = data.idValue;
-        // TODO fix this: sender reader
-        const currentuserId = data.user.id;
-        const senderId = data.partner.id;
+    const conversationId = data.idValue;
+    // TODO fix this: sender reader
+    const currentuserId = data.user.id;
+    const senderId = data.partner.id;
 
-        messageStore.activeChatId = conversationId;
+    messageStore.activeChatId = conversationId;
 
-        const socket = validateSocket();
-        if (!socket) return;
+    const socket = validateSocket();
+    if (!socket) return;
 
-        emitMarkAllRead({ conversationId, currentuserId, senderId }).catch(
-            (err) => console.error("Failed to mark as read:", err),
-        );
+    emitMarkAllRead({ conversationId, currentuserId, senderId }).catch((err) =>
+      console.error("Failed to mark as read:", err),
+    );
+  });
+
+  $effect(() => {
+    if (!messageListRef) return;
+    if (!messageIds) return;
+    messageListRef?.scrollTo({
+      top: messageListRef.scrollHeight,
+      behavior: "smooth",
     });
+  });
 
-    $effect(() => {
-        if (!messageListRef) return;
-        if (!messageIds) return;
-        messageListRef?.scrollTo({
-            top: messageListRef.scrollHeight,
-            behavior: "smooth",
-        });
-    });
-
-    function loadNewer() {
-        console.log("loading newer");
-    }
+  function loadNewer() {
+    console.log("loading newer");
+  }
 </script>
 
 <div id="chatbox" class="flex flex-col h-full">
-    <ChatHeader partner={data.partner} />
-    <div bind:this={messageListRef} class="flex-1 overflow-y-auto min-h-0">
-        <div class="bg-amber-400" use:interSectionObserver={loadOlder}>top</div>
-        {#each messageIds as id (id)}
-            {@const entry = messageStore.messages.get(id)}
-            {#if entry}
-                <div class="message flex">
-                    {#if entry.message.senderId === data.user.id}
-                        <div
-                            class="bg-blue-100 flex flex-col justify-end border max-w-1/2 ml-auto px-2 py-1"
-                        >
-                            <span id={entry.message.messageId.toString()}
-                                >{entry.message.content}</span
-                            >
-                            <span
-                                id="meta-data"
-                                class="flex justify-between items-end gap-2"
-                            >
-                                <span class="text-xs opacity-60"
-                                    >{formatLocalTime(
-                                        entry.message.createdAt,
-                                    )}</span
-                                >
-                                {@render receipt(entry.receipt)}
-                            </span>
-                        </div>
-                    {:else}
-                        <div
-                            class="bg-gray-300 flex flex-col justify-start border max-w-1/2 px-2 py-1"
-                        >
-                            <span
-                                id={entry.message.messageId.toString()}
-                                class="sender">{entry.message.content}</span
-                            >
-                            <span
-                                id="meta-data"
-                                class="flex justify-start items-start gap-2"
-                            >
-                                <span class="text-xs opacity-60">
-                                    {formatLocalTime(
-                                        entry.message.createdAt,
-                                    )}</span
-                                >
-                            </span>
-                        </div>
-                    {/if}
-                </div>
-            {/if}
-        {/each}
-        <div class="bg-amber-400" use:interSectionObserver={loadNewer}>
-            bottom
+  <ChatHeader partner={data.partner} />
+  <div bind:this={messageListRef} class="flex-1 overflow-y-auto min-h-0">
+    <div class="bg-amber-400" use:interSectionObserver={loadOlder}>top</div>
+    {#each messageIds as id (id)}
+      {@const entry = messageStore.messages.get(id)}
+      {#if entry}
+        <div class="message flex">
+          {#if entry.message.senderId === data.user.id}
+            <div
+              class="bg-blue-100 flex flex-col justify-end border max-w-1/2 ml-auto px-2 py-1"
+            >
+              <span id={entry.message.messageId.toString()}
+                >{entry.message.content}</span
+              >
+              <span id="meta-data" class="flex justify-between items-end gap-2">
+                <span class="text-xs opacity-60"
+                  >{formatLocalTime(entry.message.createdAt)}</span
+                >
+                {@render receipt(entry.receipt)}
+              </span>
+            </div>
+          {:else}
+            <div
+              class="bg-gray-300 flex flex-col justify-start border max-w-1/2 px-2 py-1"
+            >
+              <span id={entry.message.messageId.toString()} class="sender"
+                >{entry.message.content}</span
+              >
+              <span id="meta-data" class="flex justify-start items-start gap-2">
+                <span class="text-xs opacity-60">
+                  {formatLocalTime(entry.message.createdAt)}</span
+                >
+              </span>
+            </div>
+          {/if}
         </div>
-    </div>
-    <ChatInput partner={data.partner} identifier={data.identifier} />
+      {/if}
+    {/each}
+    <div class="bg-amber-400" use:interSectionObserver={loadNewer}>bottom</div>
+  </div>
+  <ChatInput partner={data.partner} identifier={data.identifier} />
 </div>
 
 {#snippet receipt(receipt: MessageReceipt)}
-    {#if receipt.status === "sent"}
-        <Check size={14} />
-    {:else if receipt.status === "delivered"}
-        <CheckCheck size={14} />
-    {:else if receipt.status === "read"}
-        <CheckCheck size={14} class="text-blue-500" />
-    {/if}
+  {#if receipt.status === "sent"}
+    <Check size={14} />
+  {:else if receipt.status === "delivered"}
+    <CheckCheck size={14} />
+  {:else if receipt.status === "read"}
+    <CheckCheck size={14} class="text-blue-500" />
+  {/if}
 {/snippet};
 
 <style>
-    /* @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap");
+  /* @import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Roboto+Slab:wght@100..900&display=swap");
 
   #chatbox {
     font-family: "Roboto Slab", serif;
     font-optical-sizing: auto;
     font-weight: 400;
     font-style: normal;
+  }
+
+  @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap");
+
+  #meta-data {
+    font-family: "IBM Plex Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
   } */
-
-    @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100..900&display=swap");
-
-    #chatbox {
-        font-family: "Roboto Slab", serif;
-        font-optical-sizing: auto;
-        font-weight: 400;
-        font-style: normal;
-    }
-
-    @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap");
-
-    #meta-data {
-        font-family: "IBM Plex Sans", sans-serif;
-        font-optical-sizing: auto;
-        font-weight: 400;
-        font-style: normal;
-    }
 </style>
--- apps/frontend/src/routes/chat/Navbar.svelte
+++ apps/frontend/src/routes/chat/Navbar.svelte
@@ -1,71 +1,71 @@
 <script lang="ts">
-    import { handleLogout } from "$lib/store/managers/entities/user.svelte";
-    import { clickOutside } from "$lib/utils/clickOutside";
-    import { CircleChevronDown } from "@lucide/svelte";
-    let { username } = $props();
-    let expandMenu = $state(false);
+  import { handleLogout } from "$lib/store/managers/entities/user.svelte";
+  import { clickOutside } from "$lib/utils/clickOutside";
+  import { CircleChevronDown } from "@lucide/svelte";
+  let { username } = $props();
+  let expandMenu = $state(false);
 
-    let isMenuExpanded = $state(false);
+  let isMenuExpanded = $state(false);
 </script>
 
 <div class="hidden justify-between items-center mx-4 lg:flex py-2 z-100">
-    <div class="flex gap-2 text-sm">
-        <a href="/" class="font-bold">Logo</a>
-        {@render link("/about", "About")}
-        {@render link("/contact", "Contact")}
-        {@render link("/feedback", "Feedback")}
-        {@render link("/support", "Support")}
-    </div>
+  <div class="flex gap-2 text-sm">
+    <a href="/" class="font-bold">Logo</a>
+    {@render link("/chat/about", "About")}
+    {@render link("/chat/contact", "Contact")}
+    {@render link("/chat/feedback", "Feedback")}
+    {@render link("/chat/support", "Support")}
+  </div>
 
-    <div class="flex items-center gap-2 ml-auto">
-        You :
-        <div class="flex relative items-center gap-1 font-medium">
-            <button
-                use:clickOutside={() => (isMenuExpanded = false)}
-                class="flex items-center gap-1 hover:bg-gray-200 px-3 py-2 rounded active:bg-gray-300"
-                onclick={() => (isMenuExpanded = !isMenuExpanded)}
-            >
-                {username}
-                <CircleChevronDown size={16} strokeWidth={2} />
-            </button>
-            <!-- MENU POPUP -->
-            {#if isMenuExpanded}
-                <div
-                    class="absolute py-1 flex flex-col top-full min-w-50 right-0 bg-white border border-gray-200 rounded z-100"
-                >
-                    {@render menuItem("/chat/view-details/me", "View details")}
-                    {@render menuItem("/chat/upgrade-to-pro", "Upgrade to Pro")}
-                    {@render menuItem("/chat/settings", "Settings")}
-                    <a
-                        title="Logout"
-                        href="/logout"
-                        data-sveltekit-preload-data={false}
-                        onclick={handleLogout}
-                        class="py-1 px-3 text-red-500 text-sm hover:bg-gray-100 active:text-red-600"
-                        >Logout</a
-                    >
-                </div>
-            {/if}
+  <div class="flex items-center gap-2 ml-auto">
+    You :
+    <div class="flex relative items-center gap-1 font-medium">
+      <button
+        use:clickOutside={() => (isMenuExpanded = false)}
+        class="flex items-center gap-1 hover:bg-gray-200 px-3 py-2 rounded active:bg-gray-300"
+        onclick={() => (isMenuExpanded = !isMenuExpanded)}
+      >
+        {username}
+        <CircleChevronDown size={16} strokeWidth={2} />
+      </button>
+      <!-- MENU POPUP -->
+      {#if isMenuExpanded}
+        <div
+          class="absolute py-1 flex flex-col top-full min-w-50 right-0 bg-white border border-gray-200 rounded z-100"
+        >
+          {@render menuItem("/chat/view-details/me", "View details")}
+          {@render menuItem("/chat/upgrade-to-pro", "Upgrade to Pro")}
+          {@render menuItem("/chat/settings", "Settings")}
+          <a
+            title="Logout"
+            href="/logout"
+            data-sveltekit-preload-data={false}
+            onclick={handleLogout}
+            class="py-1 px-3 text-red-500 text-sm hover:bg-gray-100 active:text-red-600"
+            >Logout</a
+          >
         </div>
+      {/if}
     </div>
+  </div>
 </div>
 
 <!-- snippet -->
 {#snippet link(url: string, label: string)}
-    <a
-        href={url}
-        class=" hover:text-blue-700 active:text-blue-800 underline active:bg-gray-200"
-    >
-        {label}
-    </a>
+  <a
+    href={url}
+    class=" hover:text-blue-700 active:text-blue-800 underline active:bg-gray-200"
+  >
+    {label}
+  </a>
 {/snippet}
 
 {#snippet menuItem(path: string, label: string)}
-    <a
-        title="Logout"
-        href={path}
-        data-sveltekit-preload-data={false}
-        class="py-1 px-3 text-gray-700 text-sm hover:bg-gray-100 active:text-blue-800"
-        >{label}</a
-    >
+  <a
+    title="Logout"
+    href={path}
+    data-sveltekit-preload-data={false}
+    class="py-1 px-3 text-gray-700 text-sm hover:bg-gray-100 active:text-blue-800"
+    >{label}</a
+  >
 {/snippet}
