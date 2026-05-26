4b6aae681275bfcfff942b125d137e4043c25275
VikashSurin <vikashsurin10@gmail.com>
2026-05-26 19:43:45 +0530

    fix: add Suspense boundary

--- apps/frontend/src/app/password-reset/page.tsx
+++ apps/frontend/src/app/password-reset/page.tsx
@@ -1,18 +1,25 @@
 'use client'
-
 import { Button } from "@/components/ui/button";
 import { Field, FieldGroup, FieldLabel, } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useResetPassword } from "@/src/hooks/api/useAuth";
 import { useForm } from "@tanstack/react-form";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function PasswordResetPage() {
-  const { mutate, isError, isPending, isSuccess } = useResetPassword()
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PasswordResetForm />
+    </Suspense>
+  );
+}
+
+
+function PasswordResetForm() {
   const searchParams = useSearchParams()
   const token = searchParams.get("token")
-
-  console.log({ token })
+  const { mutate, isError, isPending, isSuccess } = useResetPassword()
 
   const form = useForm({
     defaultValues: {
@@ -20,7 +27,6 @@ export default function PasswordResetPage() {
       confirmNewPassword: ""
     },
     onSubmit: async ({ value }) => {
-      if (isPending) return
       mutate({ token: token!, newPassword: value.newPassword })
     }
   })
@@ -28,72 +34,70 @@ export default function PasswordResetPage() {
   if (isSuccess) return <PasswordResetSuccess />
 
   return (
-    <section className=" h-dvh flex flex-col justify-center items-center bg-[#F8F5F2]">
-      <div className="p-4 flex flex-1 min-h-0 flex-col min-w-md justify-center gap-4 ">
-        <div>
-          <h1 className="text-3xl font-bold  mb-8">Password Reset</h1>
+    <>
+      <section className=" h-dvh flex flex-col justify-center items-center bg-[#F8F5F2]">
+        <div className="p-4 flex flex-1 min-h-0 flex-col min-w-md justify-center gap-4 ">
+          <div>
+            <h1 className="text-3xl font-bold  mb-8">Password Reset</h1>
+          </div>
+
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              form.handleSubmit()
+            }}>
+            <FieldGroup>
+              <form.Field name="newPassword">
+                {field => {
+                  const isInvalid =
+                    (field.state.meta.isTouched || form.state.isSubmitted) &&
+                    !field.state.meta.isValid;
+
+                  return (
+                    <>
+                      <Field>
+                        <FieldLabel htmlFor="newPassword">New Password</FieldLabel>
+                        <Input
+                          id="newPassword"
+                          type="password"
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          className="w-full"
+                        />
+                      </Field>
+                    </>
+                  )
+                }}
+              </form.Field>
+              <form.Field name="confirmNewPassword">
+                {field => {
+                  const isInvalid =
+                    (field.state.meta.isTouched || form.state.isSubmitted) &&
+                    !field.state.meta.isValid;
+
+                  return (
+                    <>
+                      <Field>
+                        <FieldLabel htmlFor="confirmNewPassword">Confirm New Password</FieldLabel>
+                        <Input
+                          id="confirmNewPassword"
+                          type="password"
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          className="w-full"
+                        />
+                      </Field>
+                    </>
+                  )
+                }}
+              </form.Field>
+              <Button type="submit" disabled={isPending || isSuccess}>Submit</Button>
+            </FieldGroup>
+
+          </form>
         </div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            form.handleSubmit()
-          }}>
-          <FieldGroup>
-            <form.Field name="newPassword">
-              {field => {
-                const isInvalid =
-                  (field.state.meta.isTouched || form.state.isSubmitted) &&
-                  !field.state.meta.isValid;
-
-                return (
-                  <>
-                    <Field>
-                      <FieldLabel htmlFor="newPassword">New Password</FieldLabel>
-                      <Input
-                        id="newPassword"
-                        type="password"
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        className="w-full"
-                      />
-                    </Field>
-                  </>
-                )
-              }}
-            </form.Field>
-            <form.Field name="confirmNewPassword">
-              {field => {
-                const isInvalid =
-                  (field.state.meta.isTouched || form.state.isSubmitted) &&
-                  !field.state.meta.isValid;
-
-                return (
-                  <>
-                    <Field>
-                      <FieldLabel htmlFor="confirmNewPassword">Confirm New Password</FieldLabel>
-                      <Input
-                        id="confirmNewPassword"
-                        type="password"
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        className="w-full"
-                      />
-                    </Field>
-                  </>
-                )
-              }}
-            </form.Field>
-            {/*<Button type="submit" disabled={isPending || isSuccess}>
-            {isPending ? 'Submitting...' : isSuccess ? 'Chek your mail box' : 'Submit'}
-          </Button>*/}
-
-            <Button type="submit" disabled={isPending || isSuccess}>Submit</Button>
-          </FieldGroup>
-
-        </form>
-      </div>
-    </section>
-  );
+      </section>
+    </>
+  )
 }
-
 
 function PasswordResetSuccess() {
   return (
